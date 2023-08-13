59c8676a06db6e0803199041409bd03c77e45072
Bryan Chacaj Corado <bryancorado777@gmail.com>
2023-08-12 18:16:56 -0600

    commit_py

---
+++ listas.py
@@ -0,0 +1,46 @@
+
+#istas
+        #indicex    0       1     2        3
+lista_nombres = ['tono','jose','Andrea','bryan']
+
+print(lista_nombres[0])
+
+print(lista_nombres[3])
+
+print(lista_nombres[0:3:1])
+
+nombres = lista_nombres[2]
+
+print(nombres)
+
+
+lista_vacia = []
+print(lista_vacia)
+
+lista_numeros = [1,2,3,4,5]
+
+print(lista_numeros[0])
+
+lista_mixta = [ 'bryan', 1, 'chepe',5,'mario']
+
+print(lista_mixta)
+
+lista_numeros.append(20) #agreaga una lista de numeros 
+
+print(lista_numeros)
+
+
+for cada_elemento in lista_nombres:
+    print(cada_elemento * 30)
+
+lista_vacia = []
+print(lista_vacia)
+
+for elemento_nuevo in range(1,20):
+    
+    lista_vacia.append(elemento_nuevo)
+    print(lista_vacia)
+
+
+
+
