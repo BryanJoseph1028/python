99f3cb25a41c7f5ce4f5306c8f51c3a12285e785
Bryan Joseph Chacaj Corado <72031417+BryanJoseph1028@users.noreply.github.com>
2023-08-12 18:03:52 -0600

    commit py

---
+++ ejercicios_listas.py
@@ -0,0 +1,18 @@
+#solicirar notas al usuarop
+
+lista_notas = []
+
+cantidad_notas = float(input("ingresa la cantidad de nota"))
+
+contador = 0
+while(contador < canridad_notas):
+    notas = float(input("ingrese la notas"))
+    lista_notas.append(nota)
+    contador+=1
+
+   for cada_elemento in lista_notas :
+       suma = suma+lista_notas[cada_elemento]
+
+promedio = notas/cantidad_notas
+print(promedio)
+       
